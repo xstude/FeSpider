2d3035caf0e8f21a0efc29c823b4cbf862a985e2
hengwusanren <youchuangqiba@163.com>
2016-10-17 01:45:53 +0800

    upd8: styleOfElement.

--- src/fespider.js
+++ src/fespider.js
@@ -1,3 +1,16 @@
+var util = {
+    extend: function(obj1, obj2, noCast) {
+        if (obj2 == null) return obj1;
+        if (obj1 == null) obj1 = {};
+        for (var i in obj2) {
+            if (!obj2.hasOwnProperty(i)) continue;
+            if (obj1.hasOwnProperty(i) && noCast) continue;
+            obj1[i] = obj2[i];
+        }
+        return obj1;
+    }
+};
+
 var feSpider = function(_el, url) {
     var el;
     if (_el instanceof Element) {
@@ -10,55 +23,75 @@ var feSpider = function(_el, url) {
 
     var ourterHTML = el.ourterHTML;
 
-    var css = {};
+    var rules = styleRulesOfElement(el);
+    var style = styleOfElement(el);
 
-    console.log('getComputedStyle(el): ');
-    console.log(getComputedStyle(el));
+    console.log(rules);
+
+    return style;
 };
 
-var styleOfElement = function (el) {
+var styleRulesOfElement = function (el) {
     var re = CSSUtilities.getCSSRules(
         el,
         'screen',
         'selector,properties,inheritance,media,specificity,href,index',
         true
     );
-    console.log(re);
     return re;
 };
 
-var extendObj = function(obj1, obj2, noCast) {
-    if (obj2 == null) return obj1;
-    if (obj1 == null) obj1 = {};
-    for (var i in obj2) {
-        if (!obj2.hasOwnProperty(i)) continue;
-        if (obj1.hasOwnProperty(i) && noCast) continue;
-        obj1[i] = obj2[i];
+var styleOfElement = function (el) {
+    var re = inheritedStyleOfElement(el);
+
+    var srs = styleRulesOfElement(el);
+    for(var i = 0, len = srs.length; i < len; i++) {
+        var sr = srs[i];
+        if(sr.inheritance > 0) continue;
+        var srp = sr.properties;
+        for(var j in srp) {
+            if(!srp.hasOwnProperty(j) || srp[j].status !== 'active') continue;
+            re[j] = srp[j].value;
+        }
     }
-    return obj1;
+
+    return re;
 };
 
-var styleInCssStyleDeclaration = function(csd) {
+var inheritedStyleOfElement = function(el) {
     var re = {};
-    var camel = function(s) {
-        var wz = s.split('-');
-        for (var i = 1, len = wz.length; i < len; i++) {
-            var w = wz[i];
-            if (w === '') continue;
-            wz[i] = w.charAt(0).toUpperCase() + w.substr(1);
+    var p = el.parentElement;
+    if(!p) return {};
+    var ps = styleOfElement(p);
+    for(var i = 0, len = cssInheritedPropList.length; i < len; i++) {
+        var prop = cssInheritedPropList[i];
+        if(ps[prop]) {
+            re[prop] = ps[prop];
         }
-        return wz.join('');
-    };
-    for (var j = 0; csd[j] !== ''; j++) {
-        var v = csd[camel(csd[j])];
-        if (v === 'initial') continue;
-        re[csd[j]] = v;
     }
     return re;
 };
 
 var inlineStyleOfElement = function(el) {
-    var csd = el.style;
+    var csd = el.style;  
+    var styleInCssStyleDeclaration = function(csd) {
+        var re = {};
+        var camel = function(s) {
+            var wz = s.split('-');
+            for (var i = 1, len = wz.length; i < len; i++) {
+                var w = wz[i];
+                if (w === '') continue;
+                wz[i] = w.charAt(0).toUpperCase() + w.substr(1);
+            }
+            return wz.join('');
+        };
+        for (var j = 0; csd[j] !== ''; j++) {
+            var v = csd[camel(csd[j])];
+            if (v === 'initial') continue;
+            re[csd[j]] = v;
+        }
+        return re;
+    };
     return styleInCssStyleDeclaration(csd);
 };
 
@@ -94,19 +127,66 @@ var cssPropList = {
     'align': '',
     'vertical-align': '',
     'font': '',
+    'line-height': '',
     'text-align': '',
     'text-indent': '',
     'text-decoration': '',
     'letter-spacing': '',
-    'white-space': '',
     'word-wrap': '',
     'word-break': '',
     'word-spacing': '',
+    'white-space': '',
     'cursor': '',
     'filter': '',
     'content': '',
     'box': '',
     'transition': '',
     'transform': '',
-    'animation': ''
+    'animation': '',
+    'list-style': ''
 };
+
+var cssInheritedPropList = [
+    'azimuth',
+    'border-collapse',
+    'border-spacing',
+    'caption-side',
+    'color',
+    'cursor',
+    'direction',
+    'elevation',
+    'empty-cells',
+    'font-family',
+    'font-size',
+    'font-style',
+    'font-variant',
+    'font-weight',
+    'font',
+    'letter-spacing',
+    'line-height',
+    'list-style-image',
+    'list-style-position',
+    'list-style-type',
+    'list-style',
+    'orphans',
+    'pitch-range',
+    'pitch',
+    'quotes',
+    'richness',
+    'speak-header',
+    'speak-numeral',
+    'speak-punctuation',
+    'speak',
+    'speech-rate',
+    'stress',
+    'text-align',
+    'text-indent',
+    'text-transform',
+    'visibility',
+    'voice-family',
+    'volume',
+    'white-space',
+    'widows',
+    'word-spacing'
+];
+
