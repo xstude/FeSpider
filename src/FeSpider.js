c314b46e728faa66a58f63db1aa03cd280bc6bfb
keshen <hackersk@qq.com>
2017-03-13 15:30:55 +0800

    upd8: comments.

--- src/FeSpider.js
+++ src/FeSpider.js
@@ -1,10 +1,10 @@
 /**
-* @Author: Ke Shen <godzilla>
-* @Date:   2017-03-10 09:43:57
-* @Email:  keshen@sohu-inc.com
-* @Last modified by:   godzilla
-* @Last modified time: 2017-03-10 09:43:57
-*/
+ * @Author: Ke Shen <godzilla>
+ * @Date:   2017-03-10 09:43:57
+ * @Email:  keshen@sohu-inc.com
+ * @Last modified by:   godzilla
+ * @Last modified time: 2017-03-10 09:43:57
+ */
 
 (function () {
 
